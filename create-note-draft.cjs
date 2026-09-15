0dcb4c61abc3466ff360c8811ed75998a42b306c
angler <jofi5616@yahoo.co.jp>
2026-09-15 11:20:27 +0900

    Change browser launch to headless mode

--- create-note-draft.cjs
+++ create-note-draft.cjs
@@ -2,10 +2,10 @@ const { chromium } = require('playwright');
 
 (async () => {
   const browser = await chromium.launch({
-    headless: false
+    headless: true
   });
   const context = await browser.newContext({
-    storageState: '.\\storage-state.json'
+    storageState: 'storage-state.json'
   });
   const page = await context.newPage();
   await page.goto('https://note.com/');
